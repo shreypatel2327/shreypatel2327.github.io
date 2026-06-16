ffbfc5a2a9709ade0367a93ef57c77d532a95e55
shreypatel2327 <shreypatel2122@gmail.com>
2026-06-16 14:37:37 +0530

    git init

---
+++ precache-manifest.4fcccfa6a9319528883e8492fde28624.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54448ef7fa97f3697736959eb6a74769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3f99f10db4978abc58",
+    "url": "/static/css/main.9499e77f.chunk.css"
+  },
+  {
+    "revision": "892beaae477e3b204b2b",
+    "url": "/static/js/2.c8af6b72.chunk.js"
+  },
+  {
+    "revision": "0e3f99f10db4978abc58",
+    "url": "/static/js/main.3af65d4e.chunk.js"
+  },
+  {
+    "revision": "cee8a0449be765401d53",
+    "url": "/static/js/runtime-main.c0c69116.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d32dfb0890f493cf449b22819c90b717",
+    "url": "/static/media/address_image.d32dfb08.svg"
+  },
+  {
+    "revision": "1942e93c4d37f522b54b7c92e590e1f7",
+    "url": "/static/media/animated_logo.1942e93c.svg"
+  },
+  {
+    "revision": "e0b662e4223aec841fe839ef10b92686",
+    "url": "/static/media/blogs_image.e0b662e4.svg"
+  },
+  {
+    "revision": "7dde0854e49bf8d84af001b960890114",
+    "url": "/static/media/cloud_infrastructure.7dde0854.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "a86b032c000a1c0e36133589ea71c4d6",
+    "url": "/static/media/data_science.a86b032c.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "e82f6590a47c498006cadead65a628d9",
+    "url": "/static/media/developerActivity.e82f6590.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "11e53de826abcbcd2f1d8f6c3f1332fd",
+    "url": "/static/media/education.11e53de8.svg"
+  },
+  {
+    "revision": "f7ec136b72de1f872761d01aab8795ec",
+    "url": "/static/media/experience.f7ec136b.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "6b87050d4b232b782af2a8279e9b503d",
+    "url": "/static/media/fa-brands-400.6b87050d.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "f64d896631c0a8da8df21a21dab49d09",
+    "url": "/static/media/fa-regular-400.f64d8966.svg"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "abae0fa6f52536fd6a8e4fb554950138",
+    "url": "/static/media/fa-solid-900.abae0fa6.svg"
+  },
+  {
+    "revision": "65e239975a1f0939829fea18f3257b1c",
+    "url": "/static/media/feelingProud.65e23997.svg"
+  },
+  {
+    "revision": "c2cf4baa93ad237fd893e2b4f80a322c",
+    "url": "/static/media/fullstack.c2cf4baa.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "86498aa4a99251e9a6ce361d0f28fe16",
+    "url": "/static/media/googleAssistant.86498aa4.svg"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "594a6196acdc268fae5cb248be69a163",
+    "url": "/static/media/jsFramework.594a6196.svg"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "3b387a3ac48fd80ac055334ec4ffdf24",
+    "url": "/static/media/manOnTable.3b387a3a.svg"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "7b46471c6502a1eb1cc9711809b1dcac",
+    "url": "/static/media/programmer.7b46471c.svg"
+  },
+  {
+    "revision": "7acd419cf9b3b0482110e45bc9e7e155",
+    "url": "/static/media/projects_image.7acd419c.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "a2376eda25d01e54cdb92e4358a4cd67",
+    "url": "/static/media/royal_logo.a2376eda.png"
+  },
+  {
+    "revision": "a59db56a8c55ddeaa54672bf8037eb76",
+    "url": "/static/media/shrey_profile_photo.a59db56a.png"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "eed648c73e1c221c7e6a58203e7f179c",
+    "url": "/static/media/talksCardBack.eed648c7.svg"
+  },
+  {
+    "revision": "140f6ae81653560502fdcf244002dabd",
+    "url": "/static/media/talksCardBackAlt.140f6ae8.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "25113f3602ceff19781bc6f0fab49e41",
+    "url": "/static/media/ui_ux_design.25113f36.svg"
+  },
+  {
+    "revision": "757db0af3b6b340539907e86276fb76f",
+    "url": "/static/media/undraw_coding_joxb.757db0af.svg"
+  },
+  {
+    "revision": "377292dc5f6de9ee44d30212f0d51d9c",
+    "url": "/static/media/vgec_logo.377292dc.png"
+  }
+]);
